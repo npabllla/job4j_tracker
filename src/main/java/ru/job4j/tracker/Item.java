e839b29bfbaac12f0cc8c01e16c7f38fa2be5670
Kirill Boblak <kaboblak@miem.hse.ru>
2020-11-29 00:29:32 +0300

    2. Перегрузка конструктора [#418027]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,19 +4,21 @@ public class Item {
     private int id;
     private String name;
 
-    public int getId() {
-        return id;
+    public Item() {
     }
 
-    public void setId(int id) {
+    public Item(int id) {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static void main(String[] args) {
+        Item item = new Item();
+        Item item1 = new Item(1);
+        Item item2 = new Item(2, "Kirill");
     }
 }

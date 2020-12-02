ebe503e2e4976f5f968c2073cc874a68fca7ee89
Kirill Boblak <kaboblak@miem.hse.ru>
2020-12-02 12:25:03 +0300

    Fix

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -2,6 +2,6 @@ package ru.job4j.oop;
 
 public class College {
     Freshman fm = new Freshman();
-    Student st = (Student) fm;
-    Object o = (Object) st;
+    Student st = fm;
+    Object o = st;
 }

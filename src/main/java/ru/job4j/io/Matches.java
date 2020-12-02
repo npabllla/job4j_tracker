aba75eee3fb78685138499070fd3b96a85eccd88
Kirill Boblak <kaboblak@miem.hse.ru>
2020-12-02 12:25:14 +0300

    Fix

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -7,26 +7,45 @@ public class Matches {
         Scanner in = new Scanner(System.in);
         int matches = 11;
         while (matches > 0) {
-            System.out.println("Ход игрока №1");
-            int inputFirst = Integer.valueOf(in.nextLine());
-            while (inputFirst < 1 || inputFirst > 3){
-                inputFirst = Integer.valueOf(in.nextLine());
+            /*
+            System.out.println("Ход игрока №1, вы можете взять от 1 до 3 спичек");
+                int temp = Integer.parseInt(in.nextLine());
+                if(temp <= 3 && temp >= 1) {
+                    matches -= temp;
+                } else {
+                    System.out.println("Некорректный ввод, ход переходит к игроку №2");
+                }
+            System.out.println("Оставшиеся спички: " + matches);
+            System.out.println("Ход игрока №2, вы можете взять от 1 до 3 спичек");
+            int temp1 = Integer.parseInt(in.nextLine());
+            if(temp1 <= 3 && temp1 >= 1) {
+                matches -= temp1;
+            } else {
+                System.out.println("Некорректный ввод, ход переходит к игроку №1");
             }
-            matches -= inputFirst;
+            System.out.println("Оставшиеся спички: " + matches);
+
+             */
+            System.out.println("Ход игрока №1");
+            int temp = Integer.parseInt(in.nextLine());
+            while (temp < 1 || temp > 3){
+                temp = Integer.parseInt(in.nextLine());
+            }
+            matches -= temp;
             System.out.println("Осталось: " + matches);
             if(matches >= 1 && matches <= 3){
-                System.out.println("Победил игрок №2, игра окончена.");
+                System.out.println("Победил игрок №2, игра окончена");
                 break;
             }
             System.out.println("Ход игрока №2");
-            int inputSecond = Integer.valueOf(in.nextLine());
-            while (inputSecond < 1 || inputSecond > 3){
-                inputSecond = Integer.valueOf(in.nextLine());
+            int temp1 = Integer.parseInt(in.nextLine());
+            while (temp1 < 1 || temp1 > 4){
+                temp1 = Integer.parseInt(in.nextLine());
             }
-            matches -= inputSecond;
+            matches -= temp1;
             System.out.println("Осталось: " + matches);
             if(matches >= 1 && matches <= 3) {
-                System.out.println("Победил игрок №1, игра окончена.");
+                System.out.println("Победил игрок №1, игра окончена");
                 break;
             }
         }

46f485e5ec15b5ff5e981c5e4ee4a1d4e4d5f1de
Kirill Boblak <kaboblak@miem.hse.ru>
2020-12-11 20:31:31 +0300

    2. Комбинированный компаратор [#417940]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompactorByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByName() {
+        Comparator<Job> cmpAscByName = new JobAscByName();
+        int res = cmpAscByName.compare(
+                new Job("A", 0),
+                new Job ("B", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorAscByPriority(){
+        Comparator<Job> cmpAscByPriority = new JobAscByPriority();
+        int res = cmpAscByPriority.compare(
+                new Job("Name1",0),
+                new Job("Name1",10)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByName(){
+        Comparator<Job> cmpDescByName = new JobDescByName();
+        int res = cmpDescByName.compare(
+                new Job("B",0),
+                new Job("A",0)
+        );
+        assertThat(res, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorDescByPriority(){
+        Comparator<Job> cmpDescByPriority = new JobDescByPriority();
+        int res = cmpDescByPriority.compare(
+                new Job("Name1", 10),
+                new Job("Name1", 0)
+        );
+        assertThat(res, lessThan(0));
+    }
+}

acd10fc0c1ec9f30df9b26cb49fc5faaf053d506
Kirill Boblak <kaboblak@miem.hse.ru>
2020-12-24 01:03:07 +0300

    1. Фильтрация учеников [#417776]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -13,6 +13,7 @@ public class SchoolTest {
 
     @Before
     public void setUp() {
+        students.add(new Student(-10, "SurnameLess"));
         students.add(new Student(10, "Surname1"));
         students.add(new Student(20, "Surname2"));
         students.add(new Student(30, "Surname3"));
@@ -22,12 +23,14 @@ public class SchoolTest {
         students.add(new Student(70, "Surname7"));
         students.add(new Student(80, "Surname8"));
         students.add(new Student(90, "Surname9"));
+        students.add(new Student(110, "SurnameMore"));
     }
 
     @Test
     public void whenCollectClassA() {
         School sc = new School();
-        List<Student> rsl = sc.collect(students, pr -> pr.getScore() >= 70);
+        List<Student> rsl = sc.collect(students, pr -> pr.getScore() >= 70
+                                        && pr.getScore() <=100);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(70, "Surname7"));
         expected.add(new Student(80, "Surname8"));
@@ -49,7 +52,8 @@ public class SchoolTest {
     @Test
     public void whenCollectClassC() {
         School sc = new School();
-        List<Student> rsl = sc.collect(students, pr -> pr.getScore() < 50);
+        List<Student> rsl = sc.collect(students, pr -> pr.getScore() < 50
+                                        && pr.getScore() >= 0);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(10, "Surname1"));
         expected.add(new Student(20, "Surname2"));

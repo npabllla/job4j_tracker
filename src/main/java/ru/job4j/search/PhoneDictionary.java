9451b5f4dbb8fb920417311195a11c2988b42154
Kirill Boblak <kaboblak@miem.hse.ru>
2020-12-24 14:19:09 +0300

    6. Функции высшего порядка [#417762]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private ArrayList<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -12,36 +12,11 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-        Predicate<Person> name = new Predicate<>() {
-            @Override
-            public boolean test(Person person) {
-                return person.getName().contains(key);
-            }
-        };
-        Predicate<Person> surname = new Predicate<>() {
-            @Override
-            public boolean test(Person person) {
-                return person.getSurname().contains(key);
-            }
-        };
-        Predicate<Person> address = new Predicate<>() {
-            @Override
-            public boolean test(Person person) {
-                return person.getAddress().contains(key);
-            }
-        };
-        Predicate<Person> phone = new Predicate<>() {
-            @Override
-            public boolean test(Person person) {
-                return person.getPhone().contains(key);
-            }
-        };
-        Predicate<Person> combine = new Predicate<>() {
-            @Override
-            public boolean test(Person person) {
-                return name.or(surname.or(address.or(phone))).test(person);
-            }
-        };
+        Predicate<Person> name = (Person) -> Person.getName().contains(key);
+        Predicate<Person> surname = (Person) -> Person.getSurname().contains(key);
+        Predicate<Person> address = (Person) -> Person.getAddress().contains(key);
+        Predicate<Person> phone = (Person) -> Person.getPhone().contains(key);
+        Predicate<Person> combine = name.or(surname).or(address).or(phone);
         for (Person person : persons) {
             if (combine.test(person)) {
                 result.add(person);

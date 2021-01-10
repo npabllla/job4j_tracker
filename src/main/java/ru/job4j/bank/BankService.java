5f826e8988e06481e04a45a2b16b3e2331cb4db7
Kirill Boblak <kaboblak@miem.hse.ru>
2021-01-10 16:07:47 +0300

    3. Optional в банковских переводах. [#417789]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -3,24 +3,24 @@ package ru.job4j.bank;
 import java.util.*;
 
 public class BankService {
-    private Map<Optional<User>, List<Optional<Account>>> users = new HashMap<>();
+    private Map<User, List<Account>>users = new HashMap<>();
 
-    public void addUser(Optional<User> user) {
+    public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
-    public void addAccount(String passport, Optional<Account> account) {
+    public void addAccount(String passport, Account account) {
             var user = findByPassport(passport);
-            if(user.isPresent() && !users.get(user).contains(account)){
-                users.get(user).add(account);
+            if(user.isPresent() && !users.get(user.get()).contains(account)){
+                users.get(user.get()).add(account);
             }
     }
 
     public Optional<User> findByPassport(String passport) {
         Optional<User> rsl = Optional.empty();
-        for (Optional<User> user : users.keySet()) {
-            if (user.isPresent() && user.get().getPassport().equals(passport)) {
-                rsl = user;
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                rsl = Optional.of(user);
                 break;
             }
         }
@@ -31,9 +31,9 @@ public class BankService {
        var user = findByPassport(passport);
        Optional<Account> rsl = Optional.empty();
         if(user.isPresent()) {
-            for (Optional<Account> list : users.get(user)) {
-                if (list.isPresent() && list.get().getRequisite().equals(requisite)) {
-                    rsl = list;
+            for (Account list : users.get(user.get())) {
+                if (list.getRequisite().equals(requisite)) {
+                    rsl = Optional.of(list);
                     break;
                 }
             }
--- src/test/java/ru/job4j/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -11,48 +11,48 @@ public class BankServiceTest {
 
     @Test
     public void addUser() {
-        var user = Optional.of(new User("3434", "Petr Arsentev"));
-        var bank = new BankService();
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
         bank.addUser(user);
-        assertThat(bank.findByPassport("3434"), is(user));
+        assertThat(bank.findByPassport("3434"), is(Optional.of(user)));
     }
 
     @Test
     public void whenEnterInvalidPassport() {
-        var user =  Optional.of(new User("3434", "Petr Arsentev"));
-        var bank = new BankService();
+        User user =  new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("5546", 150D)));
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
         assertThat(bank.findByRequisite("34", "5546"), is(Optional.empty()));
     }
 
     @Test
     public void addAccount() {
-        var user =  Optional.of(new User("3434", "Petr Arsentev"));
-        var bank = new BankService();
+        User user =  new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("5546", 150D)));
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
         assertThat(bank.findByRequisite("3434", "5546").get().getBalance(), is(150D));
     }
 
     @Test
     public void transferMoney() {
-        var user =  Optional.of(new User("3434", "Petr Arsentev"));
-        var bank = new BankService();
+        User user =  new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("5546", 150D)));
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("113", 50D)));
-        bank.transferMoney(user.get().getPassport(), "5546", user.get().getPassport(), "113", 150D);
-        assertThat(bank.findByRequisite(user.get().getPassport(), "113").get().getBalance(), is(200D));
+        bank.addAccount(user.getPassport(), new Account("5546", 150D));
+        bank.addAccount(user.getPassport(), new Account("113", 50D));
+        bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
+        assertThat(bank.findByRequisite(user.getPassport(), "113").get().getBalance(), is(200D));
     }
 
     @Test
     public void add2Accounts() {
-        var user =  Optional.of(new User("3434", "Petr Arsentev"));
-        var bank = new BankService();
+        User user = new User("3434", "Petr Arsentev");
+        BankService bank = new BankService();
         bank.addUser(user);
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("1111", 150D)));
-        bank.addAccount(user.get().getPassport(),Optional.of( new Account("1122", 300D)));
+        bank.addAccount(user.getPassport(), new Account("1111", 150D));
+        bank.addAccount(user.getPassport(), new Account("1122", 300D));
         assertThat(bank.findByRequisite("3434", "1122").get().getBalance(), is(300D));
     }
 }
